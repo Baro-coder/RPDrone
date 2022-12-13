f1fbe35e07b173e5b09a95bf7aa53f771cf52ee4
Baro-coder <b.szykula00@gmail.com>
2022-12-14 00:49:21 +0100

    MPU Sensor Black Box enable

--- rpdrone.py
+++ rpdrone.py
@@ -3,7 +3,7 @@ from rpdrone.sensor.mpu import MPUSensor
 
 def main():
     
-    mpu_sensor = MPUSensor(smbus_line=1, i2c_addr=0x68, refresh_freq=10)
+    mpu_sensor = MPUSensor(smbus_line=1, i2c_addr=0x68, refresh_freq=100, blackbox_data_size=100000, record_black_box=True)
     
     mpu_sensor.run()
 
