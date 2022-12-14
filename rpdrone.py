ecb921efe5706990c17612e5dfbb406ecae16ff1
Baro-coder <b.szykula00@gmail.com>
2022-12-14 23:58:56 +0100

    Motors speed change calibrating 4

--- rpdrone.py
+++ rpdrone.py
@@ -3,8 +3,8 @@ import time
 from rpdrone.sensor.mpu import MPUSensor
 from rpdrone.motors.motors import MotorsController
 
-REFRESH_RATE = 1000
-DEGREE_TOLERANT = 3
+REFRESH_RATE = 100
+DEGREE_TOLERANT = 6
 
 
 def autohover():
@@ -49,9 +49,9 @@ def autohover():
 
             time.sleep(1 / REFRESH_RATE)
             
-            counter += 1
-            if counter == 600:
-                motors_controller.set_steady_speed(motors_controller.steady_speed + 10)
+            # counter += 1
+            # if counter == 1000:
+            #     motors_controller.set_steady_speed(motors_controller.steady_speed + 10)
         
     except KeyboardInterrupt:
         print('-- STOP --')
@@ -66,10 +66,10 @@ def main():
     mpu_sensor = MPUSensor(smbus_line=1, i2c_addr=0x68)
     motors_controller = MotorsController(fr_pin=16, fl_pin=12, br_pin=21, bl_pin=20)
     
-    motors_controller.set_max_motors_speed(1500)
+    motors_controller.set_max_motors_speed(1200)
     motors_controller.set_min_motors_speed(1100)
-    motors_controller.set_steady_speed(1180)
-    motors_controller.set_acceleration(2)
+    motors_controller.set_steady_speed(1150)
+    motors_controller.set_acceleration(1)
     
     motors_controller.arm_esc()
     motors_controller.steady()
